79188481fe21b648d6c9c802dbc9aeb274f5befe
ScipioAfricanus <skmrdr1998@gmail.com>
2026-07-13 16:07:31 +0530

    Add Spring Data JPA comparison project and update week-1 exercises

---
+++ week-2/Spring Core and Maven/exercise-1/src/main/java/MainApplication.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        // 1. Load the Spring XML configuration file from the classpath
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 2. Retrieve the configured BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // 3. Call the method to test if dependency injection succeeded
+        bookService.saveBook();
+    }
+}
